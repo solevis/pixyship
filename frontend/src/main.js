ba98d4817e585c5ad3bcbd3f3814d2ab4ac88aa0
Solevis <solevis@protonmail.com>
2021-03-12 09:20:38 +0100

    Initiate new vue project

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,16 +1,8 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
-import router from './router'
+import App from './App.vue'
 
-// prevent the production tip on Vue startup
 Vue.config.productionTip = false
 
-// eslint-disable-next-line no-new
 new Vue({
-  el: '#app',
-  router,
-  components: { App },
-  template: '<App/>'
-})
+  render: h => h(App),
+}).$mount('#app')
