f6a215e40e363535072d7bab59610b6555421e13
Solevis <solevis@protonmail.com>
2021-04-04 20:15:04 +0200

    Update About page

--- frontend-v2/src/views/About.vue
+++ frontend-v2/src/views/About.vue
@@ -1,5 +1,22 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <v-card class="pa-5">
+    <v-card class="mb-4" elevation="2" outlined>
+      <v-card-title>Developpers</v-card-title>
+
+      <v-card-text>
+        <p>Main Developper: Solevis <a href="mailto:contact@pixyship.net">&lt;contact@pixyship.net&gt;</a></p>
+        <p>Contributor: Bril</p>
+        <p>First version by Sokitume</p>
+        <p><v-icon class="mr-1">mdi-github</v-icon><a href="https://github.com/solevis/pixyship/">https://github.com/solevis/pixyship/</a></p>
+      </v-card-text>
+    </v-card>
+
+    <v-card class="mb-4" elevation="2" outlined>
+      <v-card-title>Donate</v-card-title>
+
+      <v-card-text>
+        <p><a href="https://www.buymeacoffee.com/zujitujo">If you appreciate PixyShip, you can buy me a beer!</a></p>
+      </v-card-text>
+    </v-card>
+  </v-card>
 </template>
