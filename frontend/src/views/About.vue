c14f58863bc7755b253e7e2a2ee37678dab35b24
Solevis <solevis@protonmail.com>
2021-05-14 22:42:59 +0200

    Crowfundings about page

--- frontend/src/views/About.vue
+++ frontend/src/views/About.vue
@@ -1,23 +1,41 @@
 <template>
   <v-card class="px-5 pb-1">
-    <v-card-title class="overline">> About</v-card-title>
+    <v-card-title class="overline right"></v-card-title>
+    
     <v-card class="mb-4" elevation="2" outlined>
-      <v-card-title>Developpers</v-card-title>
+      <v-card-title>Donators</v-card-title>
 
       <v-card-text>
-        <p>Main Developper: Solevis <a href="mailto:contact@pixyship.net">&lt;contact@pixyship.net&gt;</a></p>
+        <p class="font-weight-bold">A special thanks to all of the <a href="https://www.okpal.com/pixyship/#/" target="_blank">crowfunding campaign</a> crontributors, and especially to:</p>
+        <p>Admiral Col, Albathoor, Alisrassa, Baradum, GudegLezat, Human Fly, Jean, JustcallmeDr, Maccinae, Matthew-II, Muinmacar, Phinie, Ruthless Rhino, Sahara Fleet, Scorpio99, ShadowfireAngel, Solphaen, Spaceman Spiff, The Flying Brick, The worst. and YankeeDood</p>
+        <div class="mt-5" @click="buyMeACoffee" style="cursor: pointer; width: 275px;">
+          <v-img
+            :src="require('../assets/img/Solevis.png')"
+            max-height="78"
+            max-width="48"
+            contain
+            class="d-inline-block"
+            style="vertical-align: middle"
+          ></v-img>
+          <p class="ml-2 font-weight-bold d-inline-block" style="vertical-align: middle">
+            You like PixyShip?
+            <br>
+            Buy my captain some coffee <v-icon>mdi-glass-mug</v-icon>
+            <br>
+            
+          </p>
+        </div>
+      </v-card-text>
+    </v-card>
+    
+    <v-card class="mb-4" elevation="2" outlined>
+      <v-card-title>Developpers</v-card-title>
+      <v-card-text>
+        <p>Main Developper: Solevis <a href="mailto:contact@pixyship.com">&lt;contact@pixyship.com&gt;</a></p>
         <p>Contributor: Bril</p>
         <p>First version by Sokitume </p>
         <p><v-icon class="mr-1">mdi-discord</v-icon><a href="https://discord.gg/R49rqhzYrh">Discord</a></p>
         <p><v-icon class="mr-1">mdi-github</v-icon><a href="https://github.com/solevis/pixyship/">GitHub</a></p>
-      </v-card-text>
-    </v-card>
-
-    <v-card class="mb-4" elevation="2" outlined>
-      <v-card-title>Donate</v-card-title>
-
-      <v-card-text>
-        <a href="https://www.buymeacoffee.com/solevis" target="_blank"><img src="https://cdn.buymeacoffee.com/buttons/v2/default-blue.png" alt="Buy Me A Beer" style="height: 60px !important;width: 217px !important;" ></a>
       </v-card-text>
     </v-card>
   </v-card>
@@ -28,5 +46,11 @@ export default {
   created() {
     document.title = 'PixyShip - ' + this.$route.name
   },
+
+  methods: {
+    buyMeACoffee () {
+      window.open('https://www.buymeacoffee.com/solevis');
+    }
+  }
 }
 </script>
