28b2cdd28c9591f0bfc37f25489263667381a3d2
solevis <solevis@protonmail.com>
2022-01-08 12:26:37 +0100

    Import daily sales (promotions, cargos, shop, rewards) and store them in DB (#63)
    * Import daily sales (promotions, cargos, shop, rewards) and store them in database
    
    * Fix Bank/DailyBundles on Home page

--- backend/models.py
+++ backend/models.py
@@ -2,7 +2,7 @@ import hashlib
 from datetime import datetime, timedelta
 from xml.etree import ElementTree
 
-from sqlalchemy import func
+from sqlalchemy import func, UniqueConstraint
 from sqlalchemy.dialects.postgresql import UUID
 
 from db import db
@@ -122,3 +122,21 @@ class Record(db.Model):
 
         db.session.add(new_record)
         db.session.commit()
+
+
+class DailySale(db.Model):
+    id = db.Column('id', db.INT, primary_key=True)
+    type = db.Column('type', db.TEXT, nullable=False)
+    type_id = db.Column('type_id', db.INT, nullable=False)
+    sale_at = db.Column('sale_at', db.TIMESTAMP, server_default=func.now())
+    sale_from = db.Column('sale_from', db.TEXT, nullable=False)
+    currency = db.Column('currency', db.TEXT, nullable=True)
+    price = db.Column('price', db.INT, nullable=True)
+
+    __tablename__ = 'daily_sale'
+    __table_args__ = (
+        UniqueConstraint('type', 'type_id', 'sale_at', 'sale_from', name='uq_daily_sale'),
+    )
+
+    def __repr__(self):
+        return '<DailySale %r>' % self.id
