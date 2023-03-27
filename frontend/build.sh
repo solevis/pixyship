ccf7ac0dd810c06add349f112cca34ceab8fe625
Solevis <solevis@protonmail.com>
2023-03-27 22:18:08 +0200

    Add staging build mode

--- frontend/build.sh
+++ frontend/build.sh
@@ -1,4 +1,20 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
+
+BUILD_ARG="build"
+
+while getopts 'sh' opt; do
+  case "$opt" in
+    s)
+      BUILD_ARG="build-staging"
+      ;;
+
+    ?|h)
+      echo "Usage: $(basename $0) [-s]"
+      exit 1
+      ;;
+  esac
+done
+shift "$(($OPTIND -1))"
 
 npx browserslist@latest --update-db
-npm run build
+npm run "${BUILD_ARG}"
