59dd2c62ca4dd6ba93593b8b87d43deb2f0b9ca0
solevis <solevis@protonmail.com>
2024-05-09 22:36:53 +0200

    Migrate clear_cache.py to flask cli command

--- backend/app/commands/__init__.py
+++ backend/app/commands/__init__.py
@@ -1,4 +1,5 @@
 from .check import check_cli
 from .importer import importer_cli
+from .cache import cache_cli
 
-__all__ = ["check_cli", "importer_cli"]
+__all__ = ["check_cli", "importer_cli", "cache_cli"]
---
+++ backend/app/commands/cache.py
@@ -0,0 +1,16 @@
+from flask import current_app
+from flask.cli import AppGroup, with_appcontext
+
+from app.ext import cache
+
+cache_cli = AppGroup("cache", help="Manage the Flask cache.")
+
+
+@cache_cli.command('clear')
+@with_appcontext
+def clear_cache_command():
+    """Clear the Flask cache."""
+    with current_app.app_context():
+        current_app.logger.info("Clearing cache...")
+        cache.clear()
+        current_app.logger.info("Cache cleared.")
