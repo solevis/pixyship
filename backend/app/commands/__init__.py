127bb72e13596917eab71b8dd18b82e8fa3d99ac
solevis <solevis@protonmail.com>
2025-01-04 20:12:14 +0100

    Reorder exports in __init__.py files for consistency

--- backend/app/commands/__init__.py
+++ backend/app/commands/__init__.py
@@ -2,4 +2,4 @@ from .cache import cache_cli
 from .check import check_cli
 from .importer import importer_cli
 
-__all__ = ["check_cli", "importer_cli", "cache_cli"]
+__all__ = ["cache_cli", "check_cli", "importer_cli"]
